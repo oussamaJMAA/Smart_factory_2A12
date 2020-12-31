e425b5530389aef3cbf93898ec005f59d74a03a4
oussamaJMAA <jemaaoussama64@gmail.com>
2020-12-31 16:13:11 +0100

    valeur ajouté 1.3( inclure la langue anglaise  )

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
+//traduction en anglais
     QTranslator t ;
     QStringList languages ;
     languages << "French" << "English" ;
